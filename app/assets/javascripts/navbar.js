a99c9b750cc361278793098045a9258cb7954e5a
Braden Polly <bpolly@covermymeds.com>
2018-03-10 18:10:53 -0500

    Fix disconnect link. Only show Save Playlist button when connected to Spotify

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,13 +1,13 @@
 $( document ).on('turbolinks:load', function() {
   $('#spotify-connection-dropdown').on('click', function(e) {
     $('#spotify-connection-dropdown').toggleClass('is-active');
-    e.preventDefault();
   });
 
-  $(document).on("click", function(event){
-        var $trigger = $('#spotify-connection-dropdown');
-        if($trigger !== event.target && !$trigger.has(event.target).length){
-            $('#spotify-connection-dropdown').removeClass('is-active');
-        }
-    });
+  $('body').click(function(e) {
+    $('#spotify-connection-dropdown').removeClass('is-active');
+  });
+
+  $('#spotify-connection-dropdown').click(function(e){
+	   e.stopPropagation();
+  });
 })
